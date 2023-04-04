81da3d18f862dbaf6d51b292e306aaf3be625cb5
andreaDiClaudio <andreadicla@gmail.com>
2023-04-04 16:09:33 +0200

    now printing correctly the scoreboard

--- Program.cs
+++ Program.cs
@@ -35,97 +35,14 @@ internal class Program
                 //Console.WriteLine(team.ToString()); TODO PUT IT BACK IN
             }
             
-            ReadMatch( SuperLigaMatchesFolder,superLigaen);
-            ReadMatch(NordicBetLigaMatchesFolder, nordicBetLigaen);
+            ReadMatch( SuperLigaMatchesFolder,superLigaen); //Refactor name?
+            ReadMatch(NordicBetLigaMatchesFolder, nordicBetLigaen); //Refactor name?
           
         } catch (Exception e)
         {
             Console.WriteLine(e);
         }
         
-    }
-
-    /*READ MATCHES*/
-    public static void ReadMatch(string csvFolder, League league) {
-        if (Directory.Exists(csvFolder)) 
-        {
-            string[] matchPath = Directory.GetFiles(csvFolder, "*.csv");
-            foreach (string match in matchPath)
-            {
-                using StreamReader reader = new(match);
-                reader.ReadLine();
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine();
-                    string[] values = line.Split(",");
-                    string[] result = values[2].Split("-");
-
-                    int homeResult = int.Parse(result[0]);
-                    int awayResult = int.Parse(result[1]);
-
-                    //Assigns the team to variables
-                    Team teamHome = league.Teams.Find(team => team.Abbreviation == values[0]);
-                    Team teamAway = league.Teams.Find(team => team.Abbreviation == values[1]);
-
-                    //Updates teams
-                    teamHome.GamesPlayed++;
-                    teamAway.GamesPlayed++;
-
-                    if (homeResult > awayResult)
-                    {
-                        teamHome.GamesWon++;
-                        teamAway.GamesLost ++;
-                        teamHome.Points += 3;
-
-                        teamHome.WinningStreak.Enqueue("W");
-                        if (teamHome.WinningStreak.Count > 5) 
-                        {
-                            teamHome.WinningStreak.Dequeue();
-                        }
-
-                        teamAway.WinningStreak.Enqueue("L");
-                        if (teamAway.WinningStreak.Count > 5) 
-                        {
-                            teamAway.WinningStreak.Dequeue();
-                        }
-                    }
-                    else if (homeResult < awayResult)
-                    {
-                        teamAway.GamesWon++;
-                        teamHome.GamesLost++;
-                        teamAway.Points += 3;
-
-                        teamHome.WinningStreak.Enqueue("L");
-                        if (teamHome.WinningStreak.Count > 5) 
-                        {
-                            teamHome.WinningStreak.Dequeue();
-                        }
-
-                        teamAway.WinningStreak.Enqueue("W");
-                        if (teamAway.WinningStreak.Count > 5) 
-                        {
-                            teamAway.WinningStreak.Dequeue();
-                        }
-                    }
-                    else
-                    {
-                        teamHome.GamesTied++;
-                        teamAway.GamesTied++;
-
-                        teamHome.Points += 1;
-                        teamAway.Points += 1;
-                    }
-                    teamHome.GoalsFor += homeResult;
-                    teamAway.GoalsFor += awayResult;
-
-                    teamHome.GoalsAgainst += awayResult;
-                    teamAway.GoalsAgainst += homeResult;
-                    
-                    Console.WriteLine($"/*{league.Name}*/");
-                    PrintTable(league);
-                }
-            }
-        }
     }
 
     public static List<League> Setup()
@@ -202,44 +119,128 @@ internal class Program
         }
     }
 
-    /*TABLE FORMATTING*/
+    // A method to read match data from CSV files and update the league standings.
+    public static void ReadMatch(string csvFolder, League league) {
+
+    if (Directory.Exists(csvFolder)) {
+        // Get all CSV files in the specified folder
+        string[] matchPath = Directory.GetFiles(csvFolder, "*.csv");
+        // Process each CSV file
+        foreach (string match in matchPath) {
+            using StreamReader reader = new(match);
+            // Skip the first line (header row)
+            reader.ReadLine();
+            // Process each line of the CSV file
+            while (!reader.EndOfStream) {
+
+                // Read a line from the CSV file and split it into an array of values
+                string line = reader.ReadLine();
+                string[] values = line.Split(",");
+                // Split the third value (match result) into home and away scores
+                string[] result = values[2].Split("-");
+                int homeResult = int.Parse(result[0]); 
+                int awayResult = int.Parse(result[1]);
+
+                // Find the home and away teams in the league by their abbreviations
+                Team teamHome = league.Teams.Find(team => team.Abbreviation == values[0]); 
+                Team teamAway = league.Teams.Find(team => team.Abbreviation == values[1]);
+
+                // Update the teams' standings based on the match result
+                teamHome.GamesPlayed++; 
+                teamAway.GamesPlayed++;
+
+                if (homeResult > awayResult) {
+                    teamHome.GamesWon++; 
+                    teamAway.GamesLost++;
+                    teamHome.Points += 3;
+                    teamHome.WinningStreak.Enqueue("W"); 
+                    if (teamHome.WinningStreak.Count > 5) { 
+                        teamHome.WinningStreak.Dequeue(); 
+                    }
+                    teamAway.WinningStreak.Enqueue("L"); 
+                    if (teamAway.WinningStreak.Count > 5) {
+                        teamAway.WinningStreak.Dequeue(); 
+                    }
+                } 
+                else if (homeResult < awayResult) {
+                    teamAway.GamesWon++; 
+                    teamHome.GamesLost++; 
+                    teamAway.Points += 3;
+                    teamHome.WinningStreak.Enqueue("L"); 
+                    if (teamHome.WinningStreak.Count > 5) { 
+                        teamHome.WinningStreak.Dequeue(); 
+                    }
+                    teamAway.WinningStreak.Enqueue("W"); 
+                    if (teamAway.WinningStreak.Count > 5) {
+                        teamAway.WinningStreak.Dequeue(); 
+                    }
+                }    
+                else 
+                {
+                    teamHome.GamesTied++; 
+                    teamAway.GamesTied++;
+                    teamHome.Points += 1; 
+                    teamAway.Points += 1; 
+                }
+
+                teamHome.GoalsFor += homeResult; 
+                teamAway.GoalsFor += awayResult;
+                teamHome.GoalsAgainst += awayResult; 
+                teamAway.GoalsAgainst += homeResult;
+
+                // Print the league standings after each match
+                Console.WriteLine($"/*{league.Name}*/"); 
+                PrintTable(league); 
+                }
+            }
+        }
+    }
+
     static void PrintTable(League league) {
-        //Prints the table header
-        Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------------------------------------------+");
+    //Prints the table header
+    Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------------------------------------------+");
+    Console.Write("|");
+    Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-9} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
+    "Pos", "Abbrev", "Mark", "Club-Name", "Games-Played", "Games-Won", "Games-Drawn", "Games-Lost",
+    "Goals-For", "Goals-Against", "Goal-Diff", "Points", "Winning-Streak");
+    Console.Write("|");
+    Console.WriteLine();
+
+    // Sort the teams by points in descending order
+    List<Team> sortedTeams = league.Teams.OrderByDescending(t => t.Points).ToList();
+
+    // Assign positions to each team based on their sorted order, checking for the same position
+    int position = 1;
+    int prevPoints = -1;
+    for (int i = 0; i < sortedTeams.Count; i++)
+    {
+        Team team = sortedTeams[i];
+        int difference = team.GoalsFor - team.GoalsAgainst;
+        string winningStreak = string.Join("|", team.WinningStreak);
         Console.Write("|");
-        Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-9} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
-            "Pos", "Abbrev", "Mark", "Club-Name", "Games-Played", "Games-Won", "Games-Drawn", "Games-Lost",
-              "Goals-For", "Goals-Against", "Goal-Diff", "Points", "Winning-Streak");
+
+        // Check if the team has the same points as the previous or next team
+        if (team.Points != prevPoints && (i == sortedTeams.Count - 1 || team.Points != sortedTeams[i + 1].Points))
+        {
+            // Assign the team the next available position
+            Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-10} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
+            position, team.Abbreviation, team.SpecialRanking, team.FullName, team.GamesPlayed, team.GamesWon, team.GamesTied, team.GamesLost,
+            team.GoalsFor, team.GoalsAgainst, difference, team.Points, winningStreak);
+            position++;
+        }
+        else
+        {
+            // Assign the team a position of '-'
+            Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-10} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
+            "-", team.Abbreviation, team.SpecialRanking, team.FullName, team.GamesPlayed, team.GamesWon, team.GamesTied, team.GamesLost,
+            team.GoalsFor, team.GoalsAgainst, difference, team.Points, winningStreak);
+        }
         Console.Write("|");
         Console.WriteLine();
+        prevPoints = team.Points;
+    }
 
-        // Sort the teams by points in descending order
-        List<Team> sortedTeams = league.Teams.OrderByDescending(t => t.Points).ToList();
-
-        // Assign positions to each team based on their sorted order
-        int position = 1;
-        int prevPoints = -1;
-        foreach (Team team in sortedTeams) 
-        {
-            int difference = team.GoalsFor - team.GoalsAgainst;
-            string winningStreak = string.Join("|", team.WinningStreak);
-            Console.Write("|");
-            if (team.Points != prevPoints)
-            {
-                Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-10} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
-                position, team.Abbreviation, team.SpecialRanking, team.FullName, team.GamesPlayed, team.GamesWon, team.GamesTied, team.GamesLost,
-                team.GoalsFor, team.GoalsAgainst, difference, team.Points, winningStreak);
-            position++;
-            } else //TODO check also for the next team isntead of only the previous
-            {
-                Console.Write("{0,-4} {1,-6} {2,-5} {3,-25} {4,-12} {5,-9} {6,-11} {7,-10} {8,-12} {9,-13} {10,-9} {11,-8} {12,-15}",
-                "-", team.Abbreviation, team.SpecialRanking, team.FullName, team.GamesPlayed, team.GamesWon, team.GamesTied, team.GamesLost,
-                team.GoalsFor, team.GoalsAgainst, difference, team.Points, winningStreak);
-            }
-            Console.Write("|");
-            Console.WriteLine();
-            prevPoints = team.Points;
-        }
-        Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------------------------------------------+");
+    // Prints the table footer
+    Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------------------------------------------+");
     }
 }
